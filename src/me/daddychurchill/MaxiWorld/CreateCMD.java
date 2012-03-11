c7d51e6fb74d11ea86b2a792cffe60e8430dc4e1
Eddie Churchill <eddie@virtualchurchill.com>
2012-03-10 17:03:10 -0800

    Left the permission testing commented out, sorry

--- src/me/daddychurchill/MaxiWorld/CreateCMD.java
+++ src/me/daddychurchill/MaxiWorld/CreateCMD.java
@@ -1,6 +1,5 @@
 package me.daddychurchill.MaxiWorld;
 
-
 import org.bukkit.entity.Player;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -18,14 +17,14 @@ public class CreateCMD implements CommandExecutor {
     {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
-//			if (player.hasPermission("maxiworld.command")) {
+			if (player.hasPermission("maxiworld.command")) {
 				player.sendMessage("Loading/creating MaxiWorld... This might take a moment...");
 				player.teleport(plugin.getMaxiWorld().getSpawnLocation());
 				return true;
-//			} else {
-//				sender.sendMessage("You do not have permission to use this command");
-//				return false;
-//			}
+			} else {
+				sender.sendMessage("You do not have permission to use this command");
+				return false;
+			}
 		} else {
 			sender.sendMessage("This command is only usable by a player");
 			return false;
